8581d1195371358fb20ae4c6260f0334c35886ba
Neha29900 <153270760+Neha29900@users.noreply.github.com>
2025-02-07 22:28:06 +0530

    remove an element from the given array

---
+++ removeDuplicates.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(nums) {
+    let count = 0, current = 1;
+    for (nums[i] !== nums[i-1]) {
+        count = 0;
+        nums[current++] = nums[i];
+
+    }
+    else { 
+        count++;
+        if (count <= 1) nums[current++] = nums[i];
+    }
+    return current;
+};
+    
