2ee90ead2b2c2f05baf643a760df49d1d95197e7
Neha29900 <153270760+Neha29900@users.noreply.github.com>
2025-03-17 18:58:02 +0530

    Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
    The three functions are:
    
    increment() increases the current value by 1 and then returns it.
    decrement() reduces the current value by 1 and then returns it.
    reset() sets the current value to init and then returns it.

---
+++ counterTwo.js
@@ -0,0 +1,22 @@
+/**
+ * @param {integer} init
+ * @return {object}{inc, dec, reset}
+ */
+
+var createCounter = function(init){
+    let current = init;
+
+    return{
+        increment: function(){
+            return ++current;
+        },
+        decrement: function(){
+            return --current;
+        }
+    };
+};
+
+const counter = createCounter(5);
+console.log(counter.increment());
+console.log(counter.reset());
+console.log(counter.decrement());
