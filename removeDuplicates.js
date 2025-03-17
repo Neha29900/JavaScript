f102b7fe939622d8a972616d99023e2703535ec1
Neha29900 <153270760+Neha29900@users.noreply.github.com>
2025-03-17 18:45:32 +0530

    create helloworld Function in javascript

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,6 +1,7 @@
 var removeDuplicates = function(nums) {
     let count = 0, current = 1;
-    for (nums[i] !== nums[i-1]) {
+    for (nums[i] !== nums[i-1])
+         {
         count = 0;
         nums[current++] = nums[i];
 
